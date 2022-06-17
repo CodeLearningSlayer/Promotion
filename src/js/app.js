d071218bbebc117018d7cc0acaff79573829cbc0
CodeLearningSlayer <rubtsov.maks2015@yandex.ru>
2022-06-17 23:31:44 +0300

    Reviews

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,10 @@
 import * as flsFunctions from "./modules/functions.js";
 import Swiper, {Pagination} from "swiper";
+import $ from "jquery";
+import "jquery";
+import raterJs from "rater-js";
+import "raty-js";
+
 const blogSwiper = new Swiper (".swiper", {
     loop: true,
     modules: [Pagination],
@@ -11,6 +16,25 @@ const blogSwiper = new Swiper (".swiper", {
         el: '.swiper-pagination',
     },
 })
+
+// var myRating = raterJs( {
+//     element:document.querySelector(".rate"),
+//     rateCallback:function rateCallback(rating, done) {
+//       this.setRating(rating); 
+//       done(); 
+//     },
+//     readOnly:true,
+// });
+
+// myRating.setRating(4);
+
+$('.rate').raty({
+    number : 5,
+    starOn: "../img/icons/starOn.svg",
+    starOff: "../img/icons/starOff.svg",
+    readOnly: true,
+    score: 4,
+});
 
 
 flsFunctions.testWebP();
