5fd973e8357d51dbd1e78276393360e8dd46b691
CodeLearningSlayer <rubtsov.maks2015@yandex.ru>
2022-06-19 20:55:56 +0300

    All pages done

--- src/js/app.js
+++ src/js/app.js
@@ -17,16 +17,6 @@ const blogSwiper = new Swiper (".swiper", {
     },
 })
 
-// var myRating = raterJs( {
-//     element:document.querySelector(".rate"),
-//     rateCallback:function rateCallback(rating, done) {
-//       this.setRating(rating); 
-//       done(); 
-//     },
-//     readOnly:true,
-// });
-
-// myRating.setRating(4);
 
 $('.rate').raty({
     number : 5,
@@ -35,6 +25,40 @@ $('.rate').raty({
     readOnly: true,
     score: 4,
 });
+
+let questions =  document.querySelectorAll(".questions__item");
+questions.forEach((element) => {
+    const toggleText = element.querySelector(".question__text");
+    const answer = element.querySelector(".question__answer");
+    const icon = element.querySelector(".question__icon")
+    toggleText.addEventListener("click", function(){
+    (answer.classList.contains("question__answer--toggled"))? answer.classList.remove("question__answer--toggled") : answer.classList.add("question__answer--toggled");
+    (icon.classList.contains("question__icon--toggled"))? icon.classList.remove("question__icon--toggled") : icon.classList.add("question__icon--toggled");
+
+})
+})
+
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 56.9972, lng:  40.9714 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 15,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+  
+  window.initMap = initMap;
+
+  const themeButton = document.querySelector(".user-nav__theme");
+  themeButton.addEventListener("click", function(){
+    (document.body.classList.contains("dark-theme")) ? document.body.classList.remove("dark-theme") : document.body.classList.add("dark-theme");
+  })
 
 
 flsFunctions.testWebP();
